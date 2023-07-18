6c7132dfbf07026601ffcc268070ba3844489008
Mike Norton <mdnorton@pbs.org>
2023-07-18 07:10:26 -0400

    ready for slack integration

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -1,8 +1,34 @@
 import { APIGatewayEvent, Context } from 'aws-lambda'
+import * as dotenv from 'dotenv'
+import { App, ExpressReceiver } from '@slack/bolt'
+
+dotenv.config();
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  processBeforeResponse: true
+});
+
+const app = new App({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  token: `${process.env.SLACK_BOT_TOKEN}`,
+  receiver: expressReceiver
+});
+
+function parseRequestBody(stringBody: string | null) {
+  try {
+    return JSON.parse(stringBody ?? "");
+  } catch {
+    return undefined;
+  }
+}
 
 export async function handler (event: APIGatewayEvent, context: Context) {
-  return {
-    statusCode: 200,
-    body: "Hello, World"
-  };
+  const payload = parseRequestBody(event.body);
+  if(payload && payload.type && payload.type === 'url_verification') {
+    return {
+      statusCode: 200,
+      body: payload.challenge
+    };
+  }
 }
