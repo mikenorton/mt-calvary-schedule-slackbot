af05667c3cae9ae191ca894516f02e5ef2f287e5
Mike Norton <mdnorton@pbs.org>
2023-07-18 08:12:36 -0400

    respond with a wave

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent, Context } from 'aws-lambda'
 import * as dotenv from 'dotenv'
-import { App, ExpressReceiver } from '@slack/bolt'
+import { App, ExpressReceiver, ReceiverEvent } from '@slack/bolt'
 
 dotenv.config();
 
@@ -23,6 +23,10 @@ function parseRequestBody(stringBody: string | null) {
   }
 }
 
+app.message(async ({ say }) => {
+  await say("Hi :wave:");
+});
+
 // Main Code
 export async function handler (event: APIGatewayEvent, context: Context) {
   const payload = parseRequestBody(event.body);
@@ -32,6 +36,22 @@ export async function handler (event: APIGatewayEvent, context: Context) {
       body: payload.challenge
     };
   }
+
+  const slackEvent: ReceiverEvent = {
+    body: payload,
+    ack: async (response) => {
+      return new Promise<void>((resolve, reject) => {
+        resolve();
+        return {
+          statusCode: 200,
+          body: response ?? ""
+        };
+      });
+    },
+  };
+
+  await app.processEvent(slackEvent);
+  
   return {
     statusCode: 200,
     body: ""
